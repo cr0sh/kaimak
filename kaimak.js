897e55953ed21669cdfb719cd1e731fd61974d06
Nam Junghyun <13118114+cr0sh@users.noreply.github.com>
2022-04-18 14:50:01 +0900

    Rollback to old metadata V2 method
    V3 does not allow inline scripting due to CSP. As Metamask uses V2, so we.

--- kaimak.js
+++ kaimak.js
@@ -1,7 +1,9 @@
 // https://stackoverflow.com/a/9517879
 
 var s = document.createElement('script');
-s.src = chrome.runtime.getURL('inject.js');
+// TODO: This won't work on metadata V3 because of CSP. Should follow Metamask's V3 integration. ref: https://github.com/MetaMask/metamask-extension/issues/13411
+s.textContent = `(${injectKaimak})();`;
+s.setAttribute("async", "false");
 s.onload = function () {
     this.remove();
 };
@@ -9,5 +11,60 @@ s.onload = function () {
 var m = document.createElement('meta');
 m.setAttribute("kaimak-extension-id", chrome.runtime.id);
 
-(document.head || document.documentElement).appendChild(s);
-(document.head || document.documentElement).appendChild(m);
+let container = (document.head || document.documentElement);
+container.insertBefore(s, container.children[0]);
+container.appendChild(m);
+
+function injectKaimak() {
+    let __kaimak_enable = window.ethereum.enable;
+    let __kaimak_installed = false;
+    let __kaimak_kaikas = {
+        isKaikas: true,
+        _kaikas: new Proxy({
+            isEnabled: function () {
+                return new Promise((resolve, _) => resolve(true));
+            },
+            isApproved: function () {
+                return new Promise((resolve, _) => resolve(true));
+            },
+            isUnlocked: function () {
+                return window.ethereum._metamask.isUnlocked().then((x) => {
+                    console.log(`isUnlocked: ${x}`);
+                    return x;
+                });
+            }
+        }, { get: (target, name) => { return target[name] } }),
+        off: window.ethereum.removeListener.bind(window.ethereum),
+        enable: (function () {
+            if (!__kaimak_installed) {
+                __kaimak_installed = true;
+                return __kaimak_enable().then((addrs) => {
+                    window.klaytn.selectedAddress = addrs[0];
+                    window.klaytn.emit("accountsChanged");
+                    return addrs;
+                });
+            }
+            return __kaimak_enable();
+        }).bind(window.ethereum),
+        sendAsync: function (options, callback) {
+            let options_ = { ...options };
+            options_.method = options_.method.replace("klay", "eth");
+            window.ethereum.request(options_, callback);
+        }
+    };
+    let __kaimak_handler = {
+        get: function (target, name) {
+            if (name in target) {
+                return target[name];
+            }
+            return window.ethereum[name];
+        }
+    };
+
+    window.klaytn = new Proxy(__kaimak_kaikas, __kaimak_handler);
+    window.caver = {
+        klay: {
+            sendTransaction: (params) => new Promise((resolve) => window.klaytn.sendAsync({ method: "klay_sendTransaction", params: [params] }, resolve)),
+        }
+    }
+}
